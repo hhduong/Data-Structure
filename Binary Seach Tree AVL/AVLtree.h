d9c6434aebb34ba6e92b4e150b7b744bae45dd58
hhduong <hd1171999@gmail.com>
2018-10-17 01:57:30 +0700

    Cây nhị phân tìm kiếm cân bằng AVL

--- Binary Seach Tree AVL/AVLtree.h
+++ Binary Seach Tree AVL/AVLtree.h
@@ -1,20 +1,51 @@
 template <class T>
-struct AvlNode
+struct Node
 {
 	int height; // chi so can bang
 	int data;
-	struct AvlNode<T> left;
-	struct AvlNode<T> right;
+	Node<T> *left;
+	Node<T> *right;
 };
 
 template <class T>
 class TreeAVL
 {
 private:
-	AvlNode<T> *root;
-	void AddChild(T n, Node<T> *root)
+	Node<T> *root;
+	Node<T>* AddChild(T data, Node<T> *root)
 	{
-		
+		if(root == NULL)
+		{
+			root = new Node<T>;
+			root->data = data;
+			root->height = 0;
+			root->left = NULL;
+			root->right = NULL;
+		}
+		else if(data < root->data)
+		{
+			root->left = AddChild(data, root->left);
+			if(Height(root->left) - Height(root->right) == 2)
+			{
+				if(data < root->left->data)
+					root = rotateLL(root);
+				else
+					root = RotateRL(root);
+			}
+		}
+		else if(data >  root->data)
+		{
+			root->right = AddChild(data,root->right);
+			if(Height(root->right) - Height(root->left)  == 2)
+			{
+				if(data > root->right->data)
+					root = rotateRR(root);
+				else
+					root = rotateLR(root);
+			}
+		}
+		root->height = max(Height(root->left), Height(root->right)) +1; // cap nhat la do can bang
+		return root;
 	};
 
 	Node<T>* rotateLL(Node<T>* &root) // quay don left - left
@@ -41,18 +72,58 @@ private:
 
 	Node<T>* rotateLR(Node<T>* &root) // quay kep left - right
 	{
-		root->right = SingleRightRotate(root->right);
-		return SingleLeftRotate(root);
+		root->right = rotateLL(root->right);
+		return rotateRR(root);
 	};
 
 	Node<T>* RotateRL(Node<T>* &root) // quay kep right - left
 	{
-		root->left = SingleLeftRotate(root->left); // rotate left
-		return SingleRightRotate(root); // After rotate right
+		root->left = rotateRR(root->left); // rotate left
+		return rotateLL(root); // After rotate right
+	};
+	int Height(Node<T> *root)
+	{
+		return (root == NULL ? -1 : root->height);
+		
+	};
+	void Inorder(Node<T> *root) const
+	{
+		if(root != NULL)
+		{
+			cout<<root->data<<" ";
+			Inorder(root->left);
+			Inorder(root->right);
+		}
+	};
+	void Preorder(Node<T> *root) const
+	{
+		if(root != NULL)
+		{
+	        Preorder(root->left);
+	        cout << root->data << " ";
+	        Preorder(root->right);
+	    }
 	};
 
 public:
-
+	TreeAVL()
+	{
+		root = NULL;
+	};
+	void AddChild(T data)
+	{
+		root = AddChild(data,root);
+	};
+	void Inorder() const
+	{
+		Inorder(root);
+		cout<<endl;
+	};
+	void Preorder() const
+	{
+		Preorder(root);
+		cout<<endl;
+	};
 
 
 
--- Binary Seach Tree AVL/main.cpp
+++ Binary Seach Tree AVL/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 using namespace std;
-#include "AVLtree"
+#include "AVLtree.h"
 
 int main()
 {
 	
+	TreeAVL<int> t;
+	t.AddChild(30);
+	t.AddChild(5);
+    t.AddChild(35);
+    t.AddChild(32);
+    t.AddChild(40);
+    t.AddChild(45);
+    t.Preorder();
 	return 0;
 }
